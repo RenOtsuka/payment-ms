88039e90ce4d3b0452ecc320a45c89c517a6806b
Ryan <ryansantos.delossantos121@gmail.com>
2025-03-28 14:03:20 -0500

    first commit

---
+++ src/test/java/com/payment/microservice/MicroserviceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.payment.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.payment.ms.PaymentMsApplication;
+
+@SpringBootTest(classes=PaymentMsApplication.class)
+class MicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
